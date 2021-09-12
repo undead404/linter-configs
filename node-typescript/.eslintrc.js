66616a5e938baeb6e0ddf16d4adc73078af6b33d
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2021-09-12 12:12:32 +0300

    better typescript support

--- node-typescript/.eslintrc.js
+++ node-typescript/.eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'airbnb-typescript/base',
     'plugin:editorconfig/noconflict',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
